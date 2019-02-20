606a58eb2016c8b6da6d0a50cb7846ffdc9d63b6
pablo <marce_1993_1_@hotmail.com>
2019-02-20 16:45:50 -0300

    initial commit

---
+++ Uploader/Controllers/UploadController.cs
@@ -0,0 +1,75 @@
+﻿using Ipfs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Angular5FileUpload.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost]
+        [DisableRequestSizeLimit]
+        public async Task<ActionResult> UploadFileAsync()
+        {
+            try
+            {
+                Dictionary<string, string> fileHashes = new Dictionary<string, string>();
+                foreach (var file in Request.Form.Files)
+                {
+                    string folderName = "Upload";
+                    string webRootPath = _hostingEnvironment.WebRootPath;
+                    string newPath = Path.Combine(webRootPath, folderName);
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+                    if (file.Length > 0)
+                    {
+                        string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        string fullPath = Path.Combine(newPath, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        fileHashes.Add(fileName, await AddToIPFSAsync(fullPath));
+                    }
+                }
+                return new OkObjectResult(fileHashes);
+            }
+            catch (System.Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+
+        private async Task<string> AddToIPFSAsync(string fullPath)
+        {
+            using (var ipfs = new IpfsClient("http://10.244.237.128:5001"))
+            {
+                var stream = System.IO.File.OpenRead(fullPath);
+                //Name of the file to add
+                string fileName = Guid.NewGuid().ToString();
+
+                //Wrap our stream in an IpfsStream, so it has a file name.
+                IpfsStream inputStream = new IpfsStream(fileName, stream);
+
+                MerkleNode node = await ipfs.Add(inputStream);
+
+                return node.Hash.ToString();
+            }
+        }
+    }
+}
---
+++ Uploader/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Uploader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var corsBuilder = new CorsPolicyBuilder();
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowAnyMethod();
+            corsBuilder.AllowAnyOrigin();
+            //corsBuilder.WithOrigins("http://localhost:56573");
+            corsBuilder.AllowCredentials();
+
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = 1073741824;
+                x.MultipartBodyLengthLimit = 1073741824;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("SiteCorsPolicy");
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
